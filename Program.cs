dfb6d4608f01edaa9ed89407ccff788aac2b0c65
wbq <wangqiang3311@163.com>
2018-04-20 23:09:20 +0800

    vscode

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vscodeforgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
